202cc491baf9b57c146b1c2ced9c37c819f3499a
Guilherme Gusmao <guilherme.hferreira2015@gmail.com>
2024-10-11 00:30:16 -0300

    criação do AppointmentController com a função list, para buscar todos os compromissos marcardos no db e a função create para salvar um novo compromisso no banco de dados. Junto com a Entidade Appointment para representar a tabela de compromissos.

---
+++ symfony/src/Controller/AppointmentController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Appointment;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppointmentController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
+    #@Route("/api/appointments", methods={"GET"})
+    public function list(): JsonResponse
+    {
+        $appointments = $this->entityManager->getRepository(Appointment::class)->findAll();
+        return $this->json($appointments);
+    }
+
+    #@Route("/api/appointments", methods={"POST"})
+    public function create(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $appointment = new Appointment();
+        $appointment->setDate(new \DateTime($data['date']));
+
+        $this->entityManager->persist($appointment);
+        $this->entityManager->flush();
+
+        return $this->json($appointment, JsonResponse::HTTP_CREATED);
+    }
+}
